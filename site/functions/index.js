31153ee23943043648ef5978d735394747739a33
ah100101 <ah100101@gmail.com>
2019-03-03 21:50:40 -0600

    added firestore logic to functions

--- site/functions/index.js
+++ site/functions/index.js
@@ -1,23 +1,27 @@
 const functions = require('firebase-functions')
-// The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin')
 admin.initializeApp()
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
-  })
-})
+exports.updateLamp = functions.https.onRequest((req, res) => {
+  // verify color being passed in is a valid value
+  // const original = req.query.color
+  // const on = req.query.on
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+  const firestore = admin.firestore()
+  firestore
+    .collection('lamps')
+    .doc('trex')
+    .set({
+      color: '#FFF',
+      on: true
+    })
+    .then((docRef) => {
+      console.log('lamp updated')
+      console.log(docRef)
+      return res.send(200, 'T-Rex is Awake')
+    })
+    .catch(error => {
+      console.error(error)
+      return res.send(500, error)
+    })
+})
