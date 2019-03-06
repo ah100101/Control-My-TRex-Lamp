3a845fc6ddda5ea35e1fab449508a39db51f318f
ah100101 <ah100101@gmail.com>
2019-03-05 22:06:16 -0600

    added loop for checking state against device

--- agent/index.js
+++ agent/index.js
@@ -1,27 +1,71 @@
 const api = require('node-eufy-api')
+const firebase = require('firebase')
 require('dotenv').config()
 
-console.log(process.env.EUFY_USERNAME)
-console.log(process.env.EUFY_PASSWORD)
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+}
+
+firebase.initializeApp(config)
+const db = firebase.firestore()
+
+let lightState
+let deviceState = {
+  connected: false,
+  device: undefined
+}
+
+db.collection('lamps')
+  .doc('trex')
+  .onSnapshot(function (doc) {
+    lightState = doc.data()
+    console.log(lightState)
+  })
 
 api.loadDevices(process.env.EUFY_USERNAME, process.env.EUFY_PASSWORD).then(devices => {
-  devices.filter(d => d.deviceType === 'LIGHT_BULB')[0].connect()
-  
-  // console.log(details)
-  // if (details) {
-  //   let lb = api.createDevice(api.Model.T1011, details.code, details.ipAddress)
-  //   if (lb) {
-  //     lb.connect()
-  //       .then(result => {
-  //         console.log('device connected')
-  //       })
-  //       .catch(error => {
-  //         console.error(error)
-  //       })
-  //   }
-  // }
+  // connect to device
+  deviceState.device = devices.filter(d => d.deviceType === 'LIGHT_BULB')[0]
+
+  if (deviceState.device) {
+    deviceState.device.connect()
+      .then(result => {
+        console.log('device detected & connected')
+        deviceState.connected = true
+      })
+      .catch(error => console.error(error))
+  }
 })
 
-// setInterval(function () { 
-//   console.log("sup") 
-// }, 3000)
+setInterval(function () {
+  console.log(lightState)
+
+  if (deviceState.device &&
+    lightState &&
+    deviceState.connected &&
+    deviceState.device.isConnected()) {
+
+    if (lightState.on && !deviceState.device.isPowerOn()) {
+      deviceState.device.setPowerOn(true)
+        .then(() => {
+          console.log('light turned on')
+          lightState.on = true
+          deviceState.device.loadCurrentState()
+        })
+        .catch(error => console.error(error))
+    } else if (!lightState.on && deviceState.device.isPowerOn()) {
+      deviceState.device.setPowerOn(false)
+        .then(() => {
+          console.log('light turned off')
+          lightState.on = false
+          deviceState.device.loadCurrentState()
+        })
+        .catch(error => console.error(error))
+    }
+  } else {
+    console.error('device is not connected')
+  }
+}, 6000)
